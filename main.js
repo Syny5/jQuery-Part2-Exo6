86abd82573280d96ffcc992aff99865a42168eee
syn <vicdemilly@gmail.com>
2019-04-23 15:34:36 +0200

    Ajout du ready et du inherit

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-$('.color').hover(function(){
-  var colorChange = $(this).attr('class').split(' ')[1];
-  $('#text').css('color', colorChange);
-}, function(){
-  $('#text').css('color', 'black');
+$(document).ready(function(){
+  $('.color').hover(function(){
+    var colorChange = $(this).attr('class').split(' ')[1];
+    $('#text').css('color', colorChange);
+  }, function(){
+    $('#text').css('color', 'inherit');
+  });
 });
